4213d45916c0c9b8ae274a0fe4096edd185f6f16
Joe <effulge1@live.com>
2017-07-05 15:07:55 -0600

    Added Contacts schema to /models

---
+++ models/contact.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  phone: String,
+  favorite: {
+    type: Boolean,
+    default: false,
+  }
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A Contact needs a User ID'],
+  }
+});
+
+contactSchema.options.toJSON = {
+  getters: true,
+  virtuals: true,
+  transform: function(doc, ret, options) {
+    delete: ret._id
+    delete: ret.__v
+    return ret
+  }
+}
