fd3ff205ccc482d413df4ca97b71c15bfa65062c
Joe <effulge1@live.com>
2017-07-05 14:45:51 -0600

    Contacts routes

--- app.js
+++ app.js
@@ -7,6 +7,36 @@ app.use(function (req, res, next) {
   next();
 });
 
+app.get('/', function(req, res) {
+  res.json('IT WORKS!');
+});
+
+/**
+  * Contact Routes
+  */
+
+app.get('/contacts', function(req, res) {
+    res.json('Contacts - show all');
+});
+
+app.get('/contacts/:id', function(req, res) {
+    res.json('Contacts - view contact matching :id');
+});
+
+app.post('/contacts', function(req, res) {
+    res.json('Contacts - create contact');
+});
+
+app.put('/contacts', function(req, res) {
+    res.json('Contacts - update contact');
+});
+
+app.delete('/contacts/:id', function(req, res) {
+    res.json('Contacts - delete contact matching :id');
+});
+
+
+
 
 // start the server on port 1337
 app.listen(1337, function () {
